3853d67e3758192ce098f74c0862c54c333514b3
jokerwrld999 <ifalaleev49@gmail.com>
2022-12-13 02:45:34 +0200

    Clean Code Structure

--- zabbix-deploy.sh
+++ zabbix-deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# **** Messages Variables
+$success_message="OK!"
+
 # **** Get Database Variables
 while :
 do
@@ -12,7 +15,7 @@ do
         break
     elif [ $setup_type = "proxy" ]
     then
-        echo "OK!"
+        $success_message
         break
     else
         echo "Please Enter A Valid Installation Type Of Zabbix"  
@@ -24,14 +27,10 @@ sudo sed -i "s/POSTGRES_USER=.*/POSTGRES_USER=$POSTGRES_USER/" ./variables.env
 sudo sed -i "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/" ./variables.env
 sudo sed -i "s/POSTGRES_DB=.*/POSTGRES_DB=$POSTGRES_DB/" ./variables.env
 
-# **** Get Container Info
-
-host_port=$(cat $compose_file | grep -A4 -i ports | head -n2 | awk -e '{print $2}' | awk -F ':' '{print $1}')
-
-
 # **** Get Host Info
 container_status=$(docker inspect $container_name | grep Running | awk -F ":" '{print $2}' | sed 's/,.*//')
 
+# **** Cross-Distro Packages Installation
 check_pkg_manager() {
     if [ -x "$(command -v apt)" ]
     then 
@@ -44,27 +43,29 @@ check_pkg_manager() {
     fi
 }
 
+# **** Docker Anf UFW Setup
 if [ -x "$(command -v apt)" ]
 then
     echo -ne "
     -------------------------------------------------------------------------
-            Setup The Docker and UFW Security On Debian Based System....
+            Setup The Docker And UFW Security On Debian Based System....
     -------------------------------------------------------------------------
     "
-# *** Install And Enable UFW
+    # *** Install And Enable UFW
     sudo apt install -y ufw
     sudo ufw enable
-# *** Download ufw-docker Script
+    # *** Download ufw-docker Script
     sudo wget -O /usr/local/bin/ufw-docker https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
     sudo chmod +x /usr/local/bin/ufw-docker
-# *** Modify UFW Rules
+    # *** Modify UFW Rules
     ufw-docker install
-# *** Expose Ports Of The Container
+    # *** Expose Ports Of The Container
     ufw-docker allow web-nginx-pgsql
 else
-    echo "OK!"
+    $success_message
 fi
 
+# **** Docker Setup
 echo -ne "
 -------------------------------------------------------------------------
                      Checking For Docker Package....
@@ -72,18 +73,33 @@ echo -ne "
 "
 if [ -x "$(command -v docker)" ]
 then
-    echo "OK!"
+    $success_message
 else
     echo -ne "
     -------------------------------------------------------------------------
                      Installing Docker Package....
     -------------------------------------------------------------------------
     "
+    # *** Curl Install
+    if [ -x "$(command -v curl)" ]
+    then
+        $success_message
+    else
+        packagesNeeded='curl'
+        check_pkg_manager
+    fi
     curl -fsSL https://get.docker.com -o get-docker.sh
+    # *** Docker Setup
     sudo sh ./get-docker.sh
-    sudo usermod -aG docker $USER
+    if [ whoami !=root ]
+    then
+        sudo usermod -aG docker $USER
+    else
+        $success_message
+    fi
 fi
 
+# **** Docker-Compose Setup
 echo -ne "
 -------------------------------------------------------------------------
                      Checking For Docker-Compose Package....
@@ -91,24 +107,27 @@ echo -ne "
 "
 if [ -x "$(command -v docker-compose)" ]
 then
-    echo "OK!"
+    $success_message
 else
     echo -ne "
     -------------------------------------------------------------------------
                      Installing Docker-Compose Package....
     -------------------------------------------------------------------------
     "
+    # *** Docker Compose Setup
     packagesNeeded='docker-compose'
     check_pkg_manager
 fi
 
+# **** Docker-Compose Start
 echo -ne "
 -------------------------------------------------------------------------
                     Starting Zabbix Service....
 -------------------------------------------------------------------------
 "
+$1="up -d"
 compose_file="docker-compose-$setup_type.yaml"
-#docker-compose -f ./$compose_file up -d
-
+docker-compose -f ./$compose_file $1
+echo $1
 docker ps
 
